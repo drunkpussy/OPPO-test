3327c70b7c18922fc52b1a0ac2a1305cf7662d06
drunkpussy <418511740@qq.com>
2025-02-17 16:30:57 +0800

    finish scroll effect

--- src/main.js
+++ src/main.js
@@ -66,12 +66,29 @@ let tl3 = gsap.timeline({scrollTrigger: {
         // console.log(self.progress);
         // $('.zn--ksp').css('transform',  `translateY(${self.progress * 250}px)`);
         // $('.mask-2').css('opacity', self.progress * 1.5);
-        $('.design-mask').css('transform', `scale(${self.progress * 1.25 + 1})`);
+        $('.design-mask').css('transform', `scale(${self.progress * 1.3 + 1})`);
         $('.design-bg').css('transform', `scale(${ 0.75 + (1 - self.progress) * 0.25})`);
         $('.design-container').css('clip-path', `inset(34.56% 27.36% 7.27% round ${48 - 40 * (self.progress)}px)`);
     },
     onLeave: () => {
         // console.log('onLeave');
         // $('.mask-2').css('opacity', 0);
+    },
+}});
+
+// hidden card section
+let tl4 = gsap.timeline({scrollTrigger: {
+    trigger: '.hidden-card',
+    start: '95% 100%',
+    end: 'bottom bottom',
+    // markers: true,
+    // pin: true,
+    onUpdate: (self) => {
+        // console.log(self.progress);
+        $('.card').css('transform', `scale(${1.08 - self.progress * 0.08})`);
+        $('.card').css('opacity', `${self.progress}`);
+    },
+    onLeave: () => {
+        // console.log('onLeave');
     },
 }});
